0ea91033b05c1ff358dff785603414bbd91d55e2
PaarthAgarwal1 <paarthagarwal1@gmail.com>
2025-06-09 11:28:18 +0530

    print even odd funtion

---
+++ new2.java
@@ -0,0 +1,16 @@
+class Maain{
+	public static void main(String args[]){
+		int i=1;
+		System.out.println("Odd Numbers");
+		while(i<=100){
+			System.out.println(i);
+			i+=2;
+		}
+		i=2;
+		System.out.println("Even Numbers");
+		while(i<=100){
+			System.out.println(i);
+			i+=2;
+		}
+	}
+}
